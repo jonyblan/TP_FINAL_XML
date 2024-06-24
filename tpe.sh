6b0b63fb124594695b929530e4e6824a1f2cd3e9
ifrund <ifrund@itba.edu.ar>
2024-06-23 22:49:45 -0300

    Update tpe.sh
    Elimina los archivos viejos
    Comentarios para seccionar
    API key en varaible, como dice el enunciado
    Path de Saxon

--- tpe.sh
+++ tpe.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+
+
+#Delete old files
+rm -rf ./*.{xml, fo}
+rm -rf ./nascar_data.pdf
+
+#Input parameters
+year=$1
+type=$2
+
+#API Key
+SPORTRADAR_API="CPREBHi9Is8DX6DeTxVWs9ibHEHjXzUYa4pBCepc"
+
+#Error check and logging
 verifParam(){
 	if [[ $1 -lt 2013  ||  $1 -gt 2024 ]]
 	then
@@ -8,26 +22,21 @@ verifParam(){
 	then
 		return 2
 	fi
-	if [[ $# -lt 3 ]]
-	then	
-		return 3
-	fi
 	return 0
 }
 
-year=$1
-type=$2
-api_key=$3
 verifParam $* 
 error=$?
+
+#Download data if parameters are valid
 if [ $error -lt 1 ]
-then
-	
-	curl https://api.sportradar.com/nascar-ot3/${type}/${year}/drivers/list.xml?api_key=${api_key} -o drivers_list.xml
-	sleep 5s
-	curl https://api.sportradar.com/nascar-ot3/${type}/${year}/standings/drivers.xml?api_key=${api_key} -o drivers_standings.xml
+then	
+	curl https://api.sportradar.com/nascar-ot3/${type}/${year}/drivers/list.xml?api_key=${SPORTRADAR_API} -o drivers_list.xml
+	echo "2 second wait time to avoid too many requests error"
+	sleep 2s
+	curl https://api.sportradar.com/nascar-ot3/${type}/${year}/standings/drivers.xml?api_key=${SPORTRADAR_API} -o drivers_standings.xml
 fi
 	
-	basex -byear=$1 -btype=$2  -berror=${error} extract_nascar_data.xq
-	java -jar saxon-he-12.4.jar -s:nascar_data.xml -xsl:generate_fo.xsl -o:nascar_page.fo
-	fop -fo nascar_page.fo -pdf nascar_page.pdf
+basex -byear=$1 -btype=$2  -berror=${error} extract_nascar_data.xq
+java -jar ./Saxon-HE-12.4/saxon-he-12.4.jar -s:nascar_data.xml -xsl:generate_fo.xsl -o:nascar_page.fo
+fop -fo nascar_page.fo -pdf nascar_page.pdf
